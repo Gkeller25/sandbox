30e5d862f81888b22b740ffba728c968e8412ee1
Garet <kellergaret@outlook.com>
2018-06-07 00:09:21 -0600

    early addition of api for spotify functioning, returns playlists and tracks

---
+++ assets/web-api-auth-examples-master/authorization_code/public/test.js
@@ -0,0 +1,3 @@
+
+
+//curl -X "GET" "https://api.spotify.com/v1/me/playlists?limit=10&offset=5" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer BQC0g1LW45zxObHDCd1cFt4zA1Ce6pCnJAJBxtyqObnbMFW6HChCsyNZHOJCbhaprvFh-daN-g-kNq_WYv2GrS_SqEzaHNP-zIBWmv4NxUpimYphh-iZDCHteojYIQa6SrYqA12n9r6bI0xgv6ssXPFxgKKyKg"
